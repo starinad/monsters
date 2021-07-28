f9a9a4a4263eb5e7cf73b76f1592708fbab5b455
Dmitry Starina <dimastarina@gmail.com>
2021-07-28 12:10:42 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -21,9 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
---
+++ src/components/card/card.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = (props) => {
+  return (
+    <div className='card'>
+      <img
+        alt='monster'
+        src={`https://robohash.org/${props.monster.id}?set=set2`}
+      ></img>
+      <h1>{props.monster.name}</h1>
+      <h2>{props.monster.email}</h2>
+    </div>
+  );
+};
